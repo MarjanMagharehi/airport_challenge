39bd0c83419f36ee95b9b1ae5e4e41179276ea43
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-04 11:52:34 +0000

    Add test for planes to take off

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,6 +3,14 @@ require "plane.rb"
 describe Plane do
   it "instructs the plane to land at an airport" do
     plane = Plane.new
-  expect(plane).to respond_to :land
+    expect(plane).to respond_to :land
   end
+
+  it "instructs a plane to take off from an airport" do
+    plane = Plane.new
+    expect(plane).to respond to :take_off
+  end
+#
+#   # it "comfirms it is no longer at the airport" do
+#   # end
 end
