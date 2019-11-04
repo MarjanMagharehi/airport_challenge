8d1c0bd77525829451ecf934fb59aa39e53e0ac1
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-04 11:31:50 +0000

    Add first user story, test and code

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,8 @@
+require "plane.rb"
 
-Describe Plane do
-   
+describe Plane do
+  it "instructs the plane to land at an airport" do
+    plane = Plane.new
+  expect(plane).to respond_to :land
+  end
 end
