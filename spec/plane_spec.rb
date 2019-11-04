d82d8077bbe808bfb3741ba4694036367afa0eb3
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-04 12:13:37 +0000

    Add test for confirming plane is no longer in airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,9 +8,10 @@ describe Plane do
 
   it "instructs a plane to take off from an airport" do
     plane = Plane.new
-    expect(plane).to respond to :take_off
+    expect(plane).to respond_to :take_off
   end
-#
-#   # it "comfirms it is no longer at the airport" do
-#   # end
+
+  it "comfirms it is no longer at the airport" do
+    expect(plane).to respond_to :confirmed_plane_left?
+  end 
 end
