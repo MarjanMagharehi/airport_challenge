01a9eccdec5dab640281ff5abec15f9abe9aed24
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-04 15:11:17 +0000

    Refactor coding for airport being full test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,11 @@
 require 'airport.rb'
 
 describe Airport do
+  # DEFAULT_CAPACITY = 20
   it 'raises an error if the airport is full' do
     plane = Plane.new
-    expect {plane.land}.to raise_error 'Airport is full, do not land'
+    # airport.capacity.times {airport.land(Plane.new)}
+    airport.land(Plane.new)
+    expect {plane.land Plane.new}.to raise_error 'Airport is full, do not land'
   end
 end
