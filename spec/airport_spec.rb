501e1ca92c46f345e451cf74125cff234c3815a1
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-04 14:17:25 +0000

    Add extra information for raised error when airport is full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,6 +3,6 @@ require 'airport.rb'
 describe Airport do
   it 'raises an error if the airport is full' do
     plane = Plane.new
-    expect {plane.land}.to raise_error 'Airport is full'
+    expect {plane.land}.to raise_error 'Airport is full, do not land'
   end
 end
